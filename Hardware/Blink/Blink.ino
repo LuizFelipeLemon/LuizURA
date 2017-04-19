495a8d5ac5225c9d4d6db9b7f91d36b5187718cb
Luiz Felipe Santana de Araújo Souza <luizfelipeplm@gmail.com>
2017-04-19 12:19:30 -0300

    Update Blink.ino

--- Hardware/Blink/Blink.ino
+++ Hardware/Blink/Blink.ino
@@ -1,10 +1,13 @@
+
+int led = 2;    //Led conectado ao pino 2
+
 void setup() {
-  pinMode(2, OUTPUT);
+  pinMode(led, OUTPUT);     //Define o pino 2 como saída
 }
 
 void loop() {
-  digitalWrite(2, HIGH);   
-  delay(1000);              
-  digitalWrite(2, LOW);    
-  delay(1000);              
+  digitalWrite(led, HIGH);   //Escreve no pino 2 o valor alto
+  delay(1000);               //Delay de 1 segundo
+  digitalWrite(led, LOW);    //Escreve no pino 2 o valor baixo
+  delay(1000);               //Delay de 1 segundo
 }
