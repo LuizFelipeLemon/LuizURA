770a6df7223074e17d86cd9277753041aba3d51e
LuizFelipeLemon <luizfelipeplm@gmail.com>
2017-05-07 20:57:09 -0300

    PRimeiro código feito

---
+++ luiz.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#define tam 50
+ minha mãe é uma vadia
+using namespace std;
+
+
+int main(){
+  int v1[tam], x,n;
+
+
+  cout<<"Digite o numero de vendas\n"; // pede ao menos uma vez para ter um valor como comparação
+  cin>>n;
+
+
+  for(int i = 0; i < n; i++){
+    cout<<"Digite um numero\n";
+    cin>>v1[i];
+    }
+
+  int numIguais = 0;
+  for(int i = 0; i < n; i++){
+    for(int j = i-1; j >=0; j--){
+      if(v1[i]==v1[j]){
+        numIguais++;
+        break;
+      }
+    }
+  }
+
+  cout<< n - numIguais << " Numeros diferentes\n";
+}
