cc829b5d7d9b3d339036b0f0b54930964f587c8d
LuizFelipeLemon <luizfelipeplm@gmail.com>
2017-05-08 11:08:06 -0300

    updating...

--- codigo_documentado/codigo_documentado/codigo_documentado.ino
+++ codigo_documentado/codigo_documentado/codigo_documentado.ino
@@ -64,370 +64,370 @@ void loop() {
   comando = 'p';
 
   if (Serial.available()) {
-    option = Serial.read();
+    option = Serial.read();    //Passa para option o valor da leitura da serial
     Serial.println(option);
-    if (option == 'm' || option == 'a' || option == 's' || option == 'c' || option == 'u') {
-    }
-    else {
+    
+    if (option != 'c' && option != 'a' && option != 'u' && option != 'k' && option != 'r') {
       Serial.println("Opcao Invalida");
     }
-  }
 
-  if (option == 'c') {
-    digitalWrite(6, HIGH);
-    delay(500);
-    digitalWrite(6, LOW);
-    delay(500);
-    bool fast = true;
-    char aux;
-    Serial.println("Voce escolheu a opcao Manual Continua: f= para frente, t= tras, d= direita, e= esquerda, p= parar. ");
-    while (comando != 's')
-    { if (Serial.available()) {
-        comando = 0; comando = Serial.read();
+    if (option == 'c') {  //Inicia a opção manual continua
+      digitalWrite(6, HIGH);
+      delay(500);
+      digitalWrite(6, LOW);
+      delay(500);
+      bool fast = true;
+      char aux;
+      Serial.println("Voce escolheu a opcao Manual Continua: f= para frente, t= tras, d= direita, e= esquerda, p= parar. ");
+      while (comando != 's')
+      { if (Serial.available()) {
+          comando = 0; comando = Serial.read();
 
-        if (aux != comando) {
-          motor1.detach();
-          motor2.detach();
-          if (!fast) {
-            if (Serial.available()) {
-              comando = Serial.read();
-              break;
-            }
-            delay(600);
-          }
-        }
-        aux = comando;
-      }
-      int seg = 0, but = 0;
-      seg = 11;
-
-      if (comando == 'y') {
-        fast = false;
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-        motor1.write(180);
-        motor2.write(0);
-      }
-
-      if (comando == 'f' && seg > 10) {
-        fast = false;
-        seg = dist();
-        Serial.println(seg);
-        if (seg > 0 && seg <= 10 || seg < 0) {
-          motor1.detach();
-          motor2.detach();
-        }
-        else {
-          motor1.attach(SERVO1);
-          motor2.attach(SERVO4);
-        }
-        motor1.write(180);
-        motor2.write(0);
-      }
-      but = 0;
-      if (comando == 't' && but == 0 ) {
-        fast = false;
-        but = digitalRead(BOTAO);
-        Serial.println(but);
-        if (but == 0) {
-          motor1.detach();
-          motor2.detach();
-        }
-        else {
-          motor1.attach(SERVO1);
-          motor2.attach(SERVO4);
-        }
-        motor1.write(0);
-        motor2.write(180);
-      }
-      if (comando == 'b') {
-        fast = false;
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-        motor1.write(0);
-        motor2.write(180);
-      }
-      if (comando == 'e') {
-        fast = false;
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-        motor1.write(0);
-        motor2.write(0);
-      }
-      if (comando == 'd') {
-        fast = false;
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-        motor1.write(180);
-        motor2.write(180);
-      }
-      if (comando == 'p') {
-        fast = true;
-        motor1.detach();
-        motor2.detach();
-      }
-
-    }
-    Serial.println("Trocar opcao");
-  }
-
-  motor1.detach();
-  motor2.detach();
-
-  if (option == 'a') {
-    //Indicação de entrada
-    flag = 0;
-    int tempo = 0;
-    char v[TAM], n[TAM];
-    for (int i = 0; i < TAM; i++) {
-      n[i] = 'x'; v[i] = 'x';
-    }
-    while (flag == 0) {
-      while (!Serial.available());
-      Serial.setTimeout(60000);
-      Serial.readBytesUntil('x', v, TAM);
-
-      if (v[0] == 's') {
-        flag++;
-      }
-
-      for (int j = 0; v[j] != 'x' && flag == 0; j++) {
-        int w = 0;
-        if (v[j] == 'm') {
-          j++;
-          if (v[j] == '1') {
-            for (int nota = 0; nota < 156; nota++) {
-              int duracaodanota = duracaodasnotas[nota];
-              tone(BIP, melodia[nota], duracaodanota);
-              delay2(pausadepoisdasnotas[nota]);
+          if (aux != comando) {
+            motor1.detach();
+            motor2.detach();
+            if (!fast) {
+              if (Serial.available()) {
+                comando = Serial.read();
+                break;
+              }
+              delay(600);
             }
           }
-          if (v[j] == '2') {
-            tone(9, 600, 1000); delay2(1000);
-          }
+          aux = comando;
         }
-        if (v[j] >= '0' && v[j] <= '9') {
-          for (w = 0; v[j] >= '0' && v[j] <= '9'; j++ ) {
-            n[w] = v[j]; w++;
-          }
-          j = j - 1;
-          tempo = atoi(n);
-          if (v[j - w] == 'f') {
-            motor1.attach(SERVO1);
-            motor2.attach(SERVO4);
-            motor1.write(180);
-            motor2.write(0);
-            delay2(tempo);
-            motor1.detach();
-            motor2.detach();
-            delay2(DMOTOR);
-          }
-          if (v[j - w] == 't') {
-            motor1.attach(SERVO1);
-            motor2.attach(SERVO4);
-            motor1.write(0);
-            motor2.write(180);
-            delay2(tempo);
-            motor1.detach();
-            motor2.detach();
-            delay2(DMOTOR);
-          }
-          if (v[j - w] == 'd') {
-            motor1.attach(SERVO1);
-            motor2.attach(SERVO4);
-            motor1.write(180);
-            motor2.write(180);
-            delay2(tempo);
-            motor1.detach();
-            motor2.detach();
-            delay2(DMOTOR);
-          }
-          if (v[j - w] == 'e') {
-            motor1.attach(SERVO1);
-            motor2.attach(SERVO4);
-            motor1.write(0);
-            motor2.write(0);
-            delay2(tempo);
-            motor1.detach();
-            motor2.detach();
-            delay2(DMOTOR);
-          }
-          tempo = 0;
-          for (int i = 0; i < TAM; i++) {
-            n[i] = 'p';
-          }
+        int seg = 0, but = 0;
+        seg = 11;
+
+        if (comando == 'y') {
+          fast = false;
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+          motor1.write(180);
+          motor2.write(0);
         }
+
+        if (comando == 'f' && seg > 10) {
+          fast = false;
+          seg = dist();
+          Serial.println(seg);
+          if (seg > 0 && seg <= 10 || seg < 0) {
+            motor1.detach();
+            motor2.detach();
+          }
+          else {
+            motor1.attach(SERVO1);
+            motor2.attach(SERVO4);
+          }
+          motor1.write(180);
+          motor2.write(0);
+        }
+        but = 0;
+        if (comando == 't' && but == 0 ) {
+          fast = false;
+          but = digitalRead(BOTAO);
+          Serial.println(but);
+          if (but == 0) {
+            motor1.detach();
+            motor2.detach();
+          }
+          else {
+            motor1.attach(SERVO1);
+            motor2.attach(SERVO4);
+          }
+          motor1.write(0);
+          motor2.write(180);
+        }
+        if (comando == 'b') {
+          fast = false;
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+          motor1.write(0);
+          motor2.write(180);
+        }
+        if (comando == 'e') {
+          fast = false;
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+          motor1.write(0);
+          motor2.write(0);
+        }
+        if (comando == 'd') {
+          fast = false;
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+          motor1.write(180);
+          motor2.write(180);
+        }
+        if (comando == 'p') {
+          fast = true;
+          motor1.detach();
+          motor2.detach();
+        }
+
       }
+      Serial.println("Trocar opcao");
+    }
+
+    //motor1.detach();
+    //motor2.detach();
+
+    if (option == 'a') {    //Toca a melodia
+      //Indicação de entrada
+      flag = 0;
+      int tempo = 0;
+      char v[TAM], n[TAM];
       for (int i = 0; i < TAM; i++) {
         n[i] = 'x'; v[i] = 'x';
       }
-    }
-  }
-  if (option == 'u') {
-    digitalWrite(6, HIGH);
-    delay(500);
-    digitalWrite(6, LOW);
-    delay(500);
-    Serial.println("Voce escolheu a opcao Ultrassom automatica ");
-    while (comando != 's') {
-      if (Serial.available()) {
-        comando = Serial.read();
-        Serial.println(comando);
+      while (flag == 0) {
+        while (!Serial.available());
+        Serial.setTimeout(60000);
+        Serial.readBytesUntil('x', v, TAM);
+
+        if (v[0] == 's') {
+          flag++;
+        }
+
+        for (int j = 0; v[j] != 'x' && flag == 0; j++) {
+          int w = 0;
+          if (v[j] == 'm') {
+            j++;
+            if (v[j] == '1') {
+              for (int nota = 0; nota < 156; nota++) {
+                int duracaodanota = duracaodasnotas[nota];
+                tone(BIP, melodia[nota], duracaodanota);
+                delay2(pausadepoisdasnotas[nota]);
+              }
+            }
+            if (v[j] == '2') {
+              tone(9, 600, 1000); delay2(1000);
+            }
+          }
+          if (v[j] >= '0' && v[j] <= '9') {
+            for (w = 0; v[j] >= '0' && v[j] <= '9'; j++ ) {
+              n[w] = v[j]; w++;
+            }
+            j = j - 1;
+            tempo = atoi(n);
+            if (v[j - w] == 'f') {
+              motor1.attach(SERVO1);
+              motor2.attach(SERVO4);
+              motor1.write(180);
+              motor2.write(0);
+              delay2(tempo);
+              motor1.detach();
+              motor2.detach();
+              delay2(DMOTOR);
+            }
+            if (v[j - w] == 't') {
+              motor1.attach(SERVO1);
+              motor2.attach(SERVO4);
+              motor1.write(0);
+              motor2.write(180);
+              delay2(tempo);
+              motor1.detach();
+              motor2.detach();
+              delay2(DMOTOR);
+            }
+            if (v[j - w] == 'd') {
+              motor1.attach(SERVO1);
+              motor2.attach(SERVO4);
+              motor1.write(180);
+              motor2.write(180);
+              delay2(tempo);
+              motor1.detach();
+              motor2.detach();
+              delay2(DMOTOR);
+            }
+            if (v[j - w] == 'e') {
+              motor1.attach(SERVO1);
+              motor2.attach(SERVO4);
+              motor1.write(0);
+              motor2.write(0);
+              delay2(tempo);
+              motor1.detach();
+              motor2.detach();
+              delay2(DMOTOR);
+            }
+            tempo = 0;
+            for (int i = 0; i < TAM; i++) {
+              n[i] = 'p';
+            }
+          }
+        }
+        for (int i = 0; i < TAM; i++) {
+          n[i] = 'x'; v[i] = 'x';
+        }
       }
-      t = 30;
-      motor1.attach(SERVO1);
-      motor2.attach(SERVO4);
-      motor1.write(120);
-      motor2.write(0);
-      delay(300);
-      t = dist();
-      if (t > 0 && t < 23) {
-        motor1.write(40);
-        motor2.write(40);
-        delay(600);
-        motor1.detach();
-        motor2.detach();
-        delay(1000);
-        do {
-          te = dist();
-        } while (te < 0);
-        delay(100);
-        Serial.print(" aehoue: ");
-        Serial.println(te);
+    }
+
+    
+    if (option == 'u') {    //Ultrassom automatica
+      digitalWrite(6, HIGH);
+      delay(500);
+      digitalWrite(6, LOW);
+      delay(500);
+      Serial.println("Voce escolheu a opcao Ultrassom automatica ");
+      while (comando != 's') {
+        if (Serial.available()) {
+          comando = Serial.read();
+          Serial.println(comando);
+        }
+        t = 30;
         motor1.attach(SERVO1);
         motor2.attach(SERVO4);
-        motor1.write(140);
-        motor2.write(140);
-        delay(1200);
-        do {
-          td = dist();
-        } while (td < 0);
-        motor1.detach();
-        motor2.detach();
-        delay(1000);
-        /* motor1.attach(SERVO1);
-          motor2.attach(SERVO4);
+        motor1.write(120);
+        motor2.write(0);
+        delay(300);
+        t = dist();
+        if (t > 0 && t < 23) {
           motor1.write(40);
           motor2.write(40);
-          delay(600); */
-        Serial.print(" aehoud: ");
-        Serial.println(td);
-        if (te > td) {
-          motor1.attach(SERVO1);
-          motor2.attach(SERVO4);
-          motor1.write(40);
-          motor2.write(40);
-          delay(1200);
+          delay(600);
           motor1.detach();
           motor2.detach();
           delay(1000);
-        }
-        else {
+          do {
+            te = dist();
+          } while (te < 0);
+          delay(100);
+          Serial.print(" aehoue: ");
+          Serial.println(te);
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+          motor1.write(140);
+          motor2.write(140);
+          delay(1200);
+          do {
+            td = dist();
+          } while (td < 0);
+          motor1.detach();
+          motor2.detach();
+          delay(1000);
           /* motor1.attach(SERVO1);
             motor2.attach(SERVO4);
-            motor1.write(140);
-            motor2.write(140);
-            delay(600);
+            motor1.write(40);
+            motor2.write(40);
+            delay(600); */
+          Serial.print(" aehoud: ");
+          Serial.println(td);
+          if (te > td) {
+            motor1.attach(SERVO1);
+            motor2.attach(SERVO4);
+            motor1.write(40);
+            motor2.write(40);
+            delay(1200);
             motor1.detach();
-            motor2.detach(); */
+            motor2.detach();
+            delay(1000);
+          }
+          else {
+            /* motor1.attach(SERVO1);
+              motor2.attach(SERVO4);
+              motor1.write(140);
+              motor2.write(140);
+              delay(600);
+              motor1.detach();
+              motor2.detach(); */
+          }
+
+        }
+      }
+      Serial.println("Trocar opcao");
+    }
+
+    if (option == 'k') {    //Seguidor de linha
+
+      digitalWrite(6, HIGH);        //Emite um sinal sonoro
+      delay(500);
+      digitalWrite(6, LOW);
+      delay(500);
+
+      Serial.println("Voce escolheu a opcao Manual Continua: f= para frente, t= tras, d= direita, e= esquerda, p= parar. ");
+
+      int seg1, seg2;     //Variáveis para amrmazenar as leituras dos sensores de linha
+
+      while (comando != 's') {          //Permanece no laço até que o usuário digite a letra s
+        if (Serial.available()) {
+          comando = Serial.read();
+          Serial.println(comando);
+          motor1.detach();
+          motor2.detach();
+          delay(DMOTOR);
+        }
+
+        seg1 = digitalRead(LINHA1);
+        seg2 = digitalRead(LINHA2);
+        motor1.attach(SERVO1);
+        motor2.attach(SERVO4);
+        motor1.write(95);
+        motor2.write(75);
+
+        if (seg2 == 1) {            // Se o sensor conectado ao pino 3 detectou uma linha
+          motor1.detach();
+          motor2.detach();
+          delay(1000);
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+
+          while (seg2 == 1) {             //Permanece no laço até que o robô saia da linha
+            seg2 = digitalRead(LINHA2);
+            motor1.write(70);
+            motor2.write(70);
+          }
+          motor1.detach();
+          motor2.detach();
+          delay(1000);
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+        }
+
+        if (seg1 == 1) {            // Se o sensor conectado ao pino 4 detectou uma linha
+          motor1.detach();
+          motor2.detach();
+          delay(1000);
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
+
+          while (seg1 == 1) {              //Permanece no laço até que o robô saia da linha
+            seg1 = digitalRead(LINHA1);
+            motor1.write(110);
+            motor2.write(110);
+          }
+          motor1.detach();
+          motor2.detach();
+          delay(1000);
+          motor1.attach(SERVO1);
+          motor2.attach(SERVO4);
         }
 
       }
+      Serial.println("Trocar opcao");
     }
-    Serial.println("Trocar opcao");
-  }
 
-  if (option == 'k') {           //Caso o usuário tenha digitado a letra k
+    if (option == 'r') {           //Calibrar
 
-    digitalWrite(6, HIGH);        //Emite um sinal sonoro
-    delay(500);
-    digitalWrite(6, LOW);
-    delay(500);
-    
-    Serial.println("Voce escolheu a opcao Manual Continua: f= para frente, t= tras, d= direita, e= esquerda, p= parar. ");
-    
-    int seg1, seg2;     //Variáveis para amrmazenar as leituras dos sensores de linha
+      digitalWrite(6, HIGH);        //Emite um sinal sonoro
+      delay(500);
+      digitalWrite(6, LOW);
+      delay(500);
+      Serial.println("CALIBRAR ");
 
-    while (comando != 's') {          //Permanece no laço até que o usuário digite a letra s
-      if (Serial.available()) {
-        comando = Serial.read();
-        Serial.println(comando);
-        motor1.detach();
-        motor2.detach();
-        delay(DMOTOR);
-      }
-
-      seg1 = digitalRead(LINHA1);
-      seg2 = digitalRead(LINHA2);
-      motor1.attach(SERVO1);
-      motor2.attach(SERVO4);
-      motor1.write(95);
-      motor2.write(75);
-
-      if (seg2 == 1) {            // Se o sensor conectado ao pino 3 detectou uma linha
-        motor1.detach();
-        motor2.detach();
-        delay(1000);
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-        
-        while (seg2 == 1) {             //Permanece no laço até que o robô saia da linha
-          seg2 = digitalRead(LINHA2);
-          motor1.write(70);
-          motor2.write(70);
+      while (comando != 's') {      //Permanece no laço até que o usuário digite a letra s
+        if (Serial.available()) {
+          comando = Serial.read();
+          Serial.println(comando);
+          motor1.detach();
+          motor2.detach();
         }
-        motor1.detach();
-        motor2.detach();
-        delay(1000);
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-      }
-      
-      if (seg1 == 1) {            // Se o sensor conectado ao pino 4 detectou uma linha
-        motor1.detach();
-        motor2.detach();
-        delay(1000);
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-        
-        while (seg1 == 1) {              //Permanece no laço até que o robô saia da linha
-          seg1 = digitalRead(LINHA1);
-          motor1.write(110);
-          motor2.write(110);
-        }
-        motor1.detach();
-        motor2.detach();
-        delay(1000);
-        motor1.attach(SERVO1);
-        motor2.attach(SERVO4);
-      }
 
+        motor1.attach(SERVO1);
+        motor2.attach(SERVO4);
+        motor1.write(90);
+        motor2.write(90);
+
+      }
+      Serial.println("Trocar opcao");
     }
-    Serial.println("Trocar opcao");
   }
-
-  if (option == 'r') {           //Caso o usuário tenha digitado a letra r
-    
-    digitalWrite(6, HIGH);        //Emite um sinal sonoro
-    delay(500);
-    digitalWrite(6, LOW);
-    delay(500);
-    Serial.println("CALIBRAR ");
-
-    while (comando != 's') {      //Permanece no laço até que o usuário digite a letra s
-      if (Serial.available()) {
-        comando = Serial.read();
-        Serial.println(comando);
-        motor1.detach();
-        motor2.detach();
-      }
-
-      motor1.attach(SERVO1);
-      motor2.attach(SERVO4);
-      motor1.write(90);
-      motor2.write(90);
-
-    }
-    Serial.println("Trocar opcao");
-  }
-}
